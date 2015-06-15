ed5f460a133a52a62b2612524b5e2e3faf9f9a47
Jeff Schulte <jeff.d.schulte@gmail.com>
2015-06-14 22:07:24 -0700

    added devise gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '2.2.1'
 
 gem 'rails', '4.2.1'
 
@@ -11,7 +11,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem 'devise'
 
 
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #config.action below needed for the devise gem and heroku make sure you test thsi to your actual host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
