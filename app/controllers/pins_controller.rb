b6dde7968f3f4f88fcb9c741fed2eaa116c818cb
Jeff Schulte <jeff.d.schulte@gmail.com>
2015-06-21 12:55:49 -0700

    Put in permissions around who can see and edit pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,8 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   def index
     @pins = Pin.all
   end
@@ -9,14 +11,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -42,6 +44,13 @@ class PinsController < ApplicationController
     def set_pin
       @pin = Pin.find(params[:id])
     end
+
+    # For some reason, this alert shows up twice. Once in the alert field and then just below it. Don't know why.
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Edit your own pins. -Napolean Dynamite" if @pin.nil?
+    end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
