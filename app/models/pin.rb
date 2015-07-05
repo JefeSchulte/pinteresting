a09cbe0bc493771862cd1a2a17a23b5cbb2ed04a
Jeff Schulte <jeff.d.schulte@gmail.com>
2015-07-05 11:25:21 -0700

    pushing the code with the attr_accessor in it to production to see if this works in production.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,14 +1,12 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	#adding this to the file in response to an error in production.
 	#Paperclip::Error (Pin model missing required attr_accessor for 'image_file_name'):
 	#Commenting this out for now to see if this helps fix the problem of image file names not loading.
-	#attr_accessor :'image_file_name'
+	attr_accessor :image_file_name
 	
 
 end
