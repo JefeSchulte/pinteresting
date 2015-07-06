a9054089023cd666ff5782bb8adfebed43d3dd5c
Jeff Schulte <jeff.d.schulte@gmail.com>
2015-07-05 19:00:46 -0700

    Replaced email with name and added validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  	validates :description, presence: true
+	validates :image, presence: true
 
 	#adding this to the file in response to an error in production.
 	#Paperclip::Error (Pin model missing required attr_accessor for 'image_file_name'):
